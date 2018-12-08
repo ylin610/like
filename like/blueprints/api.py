199e43ffe51ed63cb60cc777d711a6f0a8c86f27
stravel <1607535392@qq.com>
2018-12-08 23:32:59 +0800

    为获取 post 的 API 添加 topic 参数

--- like/blueprints/api.py
+++ like/blueprints/api.py
@@ -13,8 +13,11 @@ api_bp = Blueprint('api', __name__, url_prefix='/api/v1')
 
 @api_bp.route('/post')
 def get_post():
-    topic = request.args.get('topic_id')
+    topic = request.args.get('topic_id', type=int)
     page = request.args.get('page', 1, type=int)
     num = request.args.get('num', current_app.config['POSTS_PER_PAGE'], type=int)
-    posts = Post.query.order_by(Post.create_time.desc()).paginate(page, num).items
+    query_obj = Post.query
+    if topic:
+        query_obj = query_obj.filter_by(topic_id=topic)
+    posts = query_obj.order_by(Post.create_time.desc()).paginate(page, num).items
     return render_template('api/post.html', posts=posts)
